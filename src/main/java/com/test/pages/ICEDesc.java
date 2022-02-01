2e4fb0811091caf49921b734f35a3d2ab7c77d24
Moorzick <testingrandomemail48@gmail.com>
2022-02-01 17:18:02 +0200

    Trying to resolve ICEDesc

--- src/main/java/com/test/pages/ICEDesc.java
+++ src/main/java/com/test/pages/ICEDesc.java
@@ -32,112 +32,125 @@ public class ICEDesc extends BasePage {
 
     private final By fieldSearch = By.xpath("//input[@id='tbFilterKey']");
 
+    private final By errorNodeExists = Tools.byFromPropertyAndValue("span", "class", "texterror");
+
 
     String directoryPath = "C:\\Users\\user\\Desktop\\";
     String fileName = "ICEDescriptions.json";
 
 
-    private By getNodeTextField (String nodeName){
+    private By getNodeTextField(String nodeName) {
         String fieldXPath = String.format("//a[text()='%s']/parent::div/parent::td/following-sibling::td/div/input", nodeName);
         return By.xpath(fieldXPath);
     }
 
-    private By getNodeSize (String nodeName){
+    private By getNodeSize(String nodeName) {
         String sizeXPath = String.format("//a[text()='%s']/parent::div/parent::td/following-sibling::td/div/span[contains(@id, 'gvItems_lblSize')]", nodeName);
         return By.xpath(sizeXPath);
     }
 
-    private By getSubgroup (String nodeName){
+    private By getSubgroup(String nodeName) {
         String sGXPath = String.format("//a[contains(@id, 'gvItems_lblNode') and text()='%s']/parent::div/parent::td/preceding-sibling::td/div/span[contains(@id, 'gvItems_lblSubGroup')]", nodeName);
         By subGroup = By.xpath(sGXPath);
         return subGroup;
     }
 
-    private String getSGText (By subGroup){
+    private String getSGText(By subGroup) {
         String text = BaseTest.driver.findElement(subGroup).getText();
         return text;
     }
 
-    private String getNodeName (int index){
+    private String getNodeName(int index) {
         String nodeNameXPath = String.format("//a[@id='gvItems_lblNode_%d']", index);
         return getAText(By.xpath(nodeNameXPath));
     }
 
-    private By getNodeByNameAndSubGroup(String nodeName, String subGroup){
+    private By getNodeByNameAndSubGroup(String nodeName, String subGroup) {
         String groupXpath = String.format("//span[contains(@id, 'gvItems_lblSubGroup') and text()='%s']/parent::div/parent::td/following-sibling::td/div/a[contains(@id, 'gvItems_lblNode') and text()='%s']", subGroup, nodeName);
         return By.xpath(groupXpath);
     }
 
-    private By getNodeByName(String nodeName){
+    private By getNodeByName(String nodeName) {
         String groupXpath = String.format("//a[contains(@id, 'gvItems_lblNode') and text()='%s']", nodeName);
         return By.xpath(groupXpath);
     }
 
-    private By getGroupByName(String groupName){
+    private By getNode(int index) {
+        String nodeNameXPath = String.format("//a[@id='gvItems_lblNode_%d']", index);
+        return By.xpath(nodeNameXPath);
+    }
+
+    private By getSubGroup(int index) {
+        String id = String.format("gvItems_lblSubGroup_%s", index);
+        return Tools.byFromPropertyAndValue("span", "id", id);
+    }
+
+
+    private By getGroupByName(String groupName) {
         String groupXpath = String.format("//span[@id='gvItems_lblGroup_0'and text()='%s']", groupName);
         return By.xpath(groupXpath);
     }
 
-    private By getSelectedGroup (){
+    private By getSelectedGroup() {
         String selectedGroupXpath = "//select[@id='ddlGroupGv']/option[@selected='selected']";
         return By.xpath(selectedGroupXpath);
     }
 
-    public ICEDesc scrapICEDescriptions () throws IOException, InterruptedException {
+    public ICEDesc scrapICEDescriptions() throws IOException, InterruptedException {
         Select groupsSelector = getDroplist(droplistGroupSelector);
         JSONArray data = new JSONArray();
-        String currentGroup= groupsSelector.getAllSelectedOptions().get(0).getText();
+        String currentGroup = groupsSelector.getAllSelectedOptions().get(0).getText();
         int optionsAmount = groupsSelector.getOptions().size();
-        for (int i=0; i<optionsAmount; i++){
-            System.out.println("Switching groups selector to option #"+i);
+        for (int i = 0; i < optionsAmount; i++) {
+            System.out.println("Switching groups selector to option #" + i);
             Thread.sleep(4000);
             getDroplist(droplistGroupSelector).selectByIndex(i);
-            if (verifyElementExist(By.xpath("//iframe"))){
+            if (verifyElementExist(By.xpath("//iframe"))) {
                 switch2Frame(By.xpath("//iframe"));
             }
-            if (i!=0){
+            if (i != 0) {
                 System.out.println("Waiting for disappearing");
                 waitForElementToDisappear(getGroupByName(currentGroup));
             }
             System.out.println("Getting new group...");
-            currentGroup=getAText(getSelectedGroup());
-            System.out.println("New current group: "+currentGroup);
+            currentGroup = getAText(getSelectedGroup());
+            System.out.println("New current group: " + currentGroup);
             JSONObject groupData = new JSONObject();
-            System.out.println("Scrapping group "+currentGroup);
+            System.out.println("Scrapping group " + currentGroup);
             groupData.put("groupName", currentGroup);
             groupData.put("nodes", scrapGroup());
             data.add(i, groupData);
-            System.out.println("Scrapping of "+currentGroup+" is completed");
+            System.out.println("Scrapping of " + currentGroup + " is completed");
             System.out.println("--------------------------------------------");
         }
-        FileWriter fw = new FileWriter(directoryPath+fileName);
+        FileWriter fw = new FileWriter(directoryPath + fileName);
         char[] output = data.toString().toCharArray();
-        System.out.println("Data: "+output);
+        System.out.println("Data: " + output);
         fw.write(output);
         fw.close();
         return Pages.iDesc();
     }
 
-    private JSONArray scrapGroup (){
+    private JSONArray scrapGroup() {
         JSONArray groupNodes = new JSONArray();
         int amount = getAllElementsCount(nodes);
-        for (int i=0; i<amount; i++){
+        for (int i = 0; i < amount; i++) {
             groupNodes.add(i, scrapNode(i));
         }
         return groupNodes;
     }
 
-    private JSONObject scrapNode (int index){
+    private JSONObject scrapNode(int index) {
         JSONObject node = new JSONObject();
-        System.out.println("Processing node #"+index);
+        System.out.println("Processing node #" + index);
         String nodeName = getNodeName(index);
-        System.out.println("Node name: "+nodeName);
+        System.out.println("Node name: " + nodeName);
         String subGroup = getSGText(getSubgroup(nodeName));
-        System.out.println("Subgroup: "+subGroup);
+        System.out.println("Subgroup: " + subGroup);
         String nodeSize = getAText(getNodeSize(nodeName));
-        System.out.println("Node size: "+nodeSize);
+        System.out.println("Node size: " + nodeSize);
         String nodeText = getFieldValue(getNodeTextField(nodeName));
-        System.out.println("Node text: "+nodeText);
+        System.out.println("Node text: " + nodeText);
 
         node.put("name", nodeName);
         node.put("subGroup", subGroup);
@@ -146,15 +159,15 @@ public class ICEDesc extends BasePage {
         return node;
     }
 
-    public ICEDesc fillICEDescs (String file) throws IOException, ParseException, InterruptedException {
+    public ICEDesc fillICEDescs(String file) throws IOException, ParseException, InterruptedException {
         Pages.icsHeader().check4Frame();
         JSONParser parser = new JSONParser();
         JSONArray data = (JSONArray) parser.parse(new FileReader(file));
-        for (int i=0; i<data.size(); i++){
+        for (int i = 0; i < data.size(); i++) {
             JSONObject group = (JSONObject) data.get(i);
             String groupName = group.get("groupName").toString();
-            System.out.println("Got new group to process: "+groupName);
-            if (!groupExist(groupName)){
+            System.out.println("Got new group to process: " + groupName);
+            if (!groupExist(groupName)) {
                 writeText(fieldGroupName, groupName);
                 click(buttonAddGroup);
             }
@@ -163,22 +176,21 @@ public class ICEDesc extends BasePage {
             getDroplist(droplistGroupSelector).selectByVisibleText(groupName);
             Thread.sleep(4000);
             System.out.println("Starting processing the nodes");
-            for (int i2=0; i2<nodes.size();i2++){
+            for (int i2 = 0; i2 < nodes.size(); i2++) {
                 JSONObject node = (JSONObject) nodes.get(i2);
                 String nodeName = node.get("name").toString();
                 String text = node.get("text").toString();
                 String subGroup;
-                if (node.get("subGroup")==null){
-                    subGroup="";
+                if (node.get("subGroup") == null) {
+                    subGroup = "";
+                } else {
+                    subGroup = node.get("subGroup").toString();
                 }
-                else {
-                    subGroup=node.get("subGroup").toString();
-                }
-                System.out.println("Group name: "+groupName);
-                System.out.println("SubGroup: "+subGroup);
-                System.out.println("Node name: "+nodeName);
-                System.out.println("Text: "+text);
-                fillDescription(subGroup, nodeName,text);
+                System.out.println("Group name: " + groupName);
+                System.out.println("SubGroup: " + subGroup);
+                System.out.println("Node name: " + nodeName);
+                System.out.println("Text: " + text);
+                fillDescription(subGroup, nodeName, text);
                 System.out.println("--------------------------------------------");
             }
             System.out.println("============================================");
@@ -187,8 +199,63 @@ public class ICEDesc extends BasePage {
     }
 
     private void fillDescription(String subGroup, String nodeName, String text) throws InterruptedException {
-        boolean nodeExists;
-        if (subGroup.equals("")){
+        boolean nodeExists = false;
+        int amount = getAllElementsCount(nodes);
+        int index = -1;
+        if (subGroup.equals("")) {
+            System.out.print("Node has no subGroup specified. nodeExist? ");
+            for (int i = 0; i < amount; i++) {
+                By currentNode = getNode(i);
+                if (getAText(currentNode).equalsIgnoreCase(nodeName)) {
+                    nodeExists = true;
+                    index = i;
+                    break;
+                }
+            }
+        }
+        else {
+            System.out.print("Node has subGroup specified: " + subGroup + ". NodeExist? ");
+            for (int i = 0; i < amount; i++) {
+                By currentNode = getNode(i);
+                By currentSubGroup = getSubGroup(i);
+                if (getAText(currentNode).equalsIgnoreCase(nodeName) && getAText(currentSubGroup).equalsIgnoreCase(subGroup)) {
+                    nodeExists = true;
+                    index = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(nodeExists);
+        if (nodeExists) {
+            String id = String.format("gvItems_tbText_%s", index);
+            By textField = Tools.inputFromId(id);
+            click(textField);
+            String initialValue = getFieldValue(textField);
+            System.out.println("Initial value: " + initialValue);
+            if (getFieldValue(textField).equals("")) {
+                System.out.println("Description is empty, filling with: " + text);
+                writeText(textField, text);
+                click(fieldSearch);
+                //Pages.icsHeader().checkForSuccess();
+                Thread.sleep(2000);
+            } else {
+                System.out.println("Description is not empty, skipping");
+            }
+        } else {
+            System.out.println("Node " + nodeName + " doesn't exist, adding...");
+            writeText(fieldNodeName, nodeName);
+            writeText(fieldSubGroup, subGroup);
+            writeText(fieldTextSize, "50");
+            click(buttonApplyNode);
+            //Pages.icsHeader().checkForSuccess();
+            Thread.sleep(2000);
+            if (!verifyElementExist(errorNodeExists)) {
+                fillDescription(subGroup, nodeName, text);
+            } else {
+                System.out.println("Node exists, skipping...");
+            }
+
+/*        if (subGroup.equals("")){
             System.out.print("Node has no subGroup specified. nodeExist? ");
             nodeExists=verifyElementExist(getNodeByName(nodeName));
             System.out.println(nodeExists);
@@ -223,11 +290,16 @@ public class ICEDesc extends BasePage {
             click(buttonApplyNode);
             //Pages.icsHeader().checkForSuccess();
             Thread.sleep(2000);
-            fillDescription(subGroup, nodeName, text);
+            if (!verifyElementExist(errorNodeExists)){
+                fillDescription(subGroup, nodeName, text);
+            }
+            else {
+                System.out.println("Node exists, skipping...");
+            }
+        }*/
         }
     }
-
-    private boolean groupExist (String group){
+    private boolean groupExist(String group) {
         String xp = String.format("//select[@id='ddlGroupGv']/option[text()='%s']", group);
         return verifyElementExist(By.xpath(xp));
     }
