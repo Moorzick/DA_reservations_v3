ed5be90807109204ebea28868296e53a52cc8656
Moorzick <testingrandomemail48@gmail.com>
2022-02-02 19:24:50 +0200

    more unsuccessful tries

--- src/main/java/com/test/pages/ICEDesc.java
+++ src/main/java/com/test/pages/ICEDesc.java
@@ -8,17 +8,21 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ICEDesc extends BasePage {
     private final By descrIFrame = By.xpath("//iframe[@id='ifmain']");
     private final By droplistGroupSelector = By.xpath("//select[@id='ddlGroupGv']");
     private final By subGroupDDown = By.xpath("//select[@id='ddlSubGroup']");
-    private final By nodes = By.xpath("//table[@id='gvItems']/tbody/tr/td/div/a");
+    private final By nodes = By.xpath("//a[contains(@id, 'gvItems_lblNode')]");
+    private final By subGroups = By.xpath("//span[contains(@id,'gvItems_lblSubGroup')]");
     private final By nodesText = By.xpath("//input[contains(@id,'gvItems_tbText')]");
 
     private final By droplistGroupCNode = By.xpath("//select[@id='ddlGroup']");
@@ -190,7 +194,20 @@ public class ICEDesc extends BasePage {
                 System.out.println("SubGroup: " + subGroup);
                 System.out.println("Node name: " + nodeName);
                 System.out.println("Text: " + text);
-                fillDescription(subGroup, nodeName, text);
+                List<WebElement> currentNodes = getAllElements(this.nodes);
+                ArrayList<String> nodesText = new ArrayList<>(currentNodes.size());
+                for (int i3=0; i3<currentNodes.size(); i3++) {
+                    String currentNodeText = currentNodes.get(i3).getText();
+                    nodesText.add(i3, currentNodeText);
+                }
+
+                List<WebElement> currentSG = getAllElements(this.subGroups);
+                ArrayList<String>  subGroupsText = new ArrayList<>(currentNodes.size());
+                for (int i4=0; i4<currentSG.size(); i4++) {
+                    String currentSGText = currentSG.get(i4).getText();
+                    subGroupsText.add(i4, currentSGText);
+                }
+                fillDescription(nodesText, subGroupsText, subGroup, nodeName, text);
                 System.out.println("--------------------------------------------");
             }
             System.out.println("============================================");
@@ -198,10 +215,67 @@ public class ICEDesc extends BasePage {
         return Pages.iDesc();
     }
 
-    private void fillDescription(String subGroup, String nodeName, String text) throws InterruptedException {
+    private void fillDescription(ArrayList<String> currentNodesText, ArrayList<String> currentSubgroups, String subGroup, String nodeName, String text) throws InterruptedException {
         boolean nodeExists = false;
-        int amount = getAllElementsCount(nodes);
-        int index = -1;
+        int index=-1;
+        if (subGroup.equals("")) {
+            System.out.print("Node has no subGroup specified. nodeExist? ");
+            for (int i = 0; i < currentNodesText.size(); i++) {
+                if (currentNodesText.get(i).equalsIgnoreCase(nodeName)) {
+                    nodeExists = true;
+                    index = i;
+                    currentNodesText.remove(i);
+                    break;
+                }
+            }
+        }
+        else {
+            System.out.print("Node has subGroup specified: " + subGroup + ". NodeExist? ");
+            for (int i = 0; i < currentNodesText.size(); i++) {
+                if (currentNodesText.get(i).equalsIgnoreCase(nodeName) && currentSubgroups.get(i).equalsIgnoreCase(subGroup)) {
+                    currentNodesText.remove(i);
+                    currentSubgroups.remove(i);
+                    nodeExists = true;
+                    index = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(nodeExists);
+
+        if (nodeExists) {
+            String id = String.format("gvItems_tbText_%s", index);
+            By textField = Tools.inputFromId(id);
+            click(textField);
+            String initialValue = getFieldValue(textField);
+            System.out.println("Initial value: " + initialValue);
+            if (getFieldValue(textField).equals("")) {
+                System.out.println("Description is empty, filling with: " + text);
+                writeText(textField, text);
+                click(fieldSearch);
+                //Pages.icsHeader().checkForSuccess();
+                Thread.sleep(2000);
+            } else {
+                System.out.println("Description is not empty, skipping");
+            }
+        } else {
+            System.out.println("Node " + nodeName + " doesn't exist, adding...");
+            writeText(fieldNodeName, nodeName);
+            writeText(fieldSubGroup, subGroup);
+            writeText(fieldTextSize, "50");
+            click(buttonApplyNode);
+            //Pages.icsHeader().checkForSuccess();
+            Thread.sleep(2000);
+            if (!verifyElementExist(errorNodeExists)) {
+                fillDescription(currentNodesText, currentSubgroups, subGroup, nodeName, text);
+            } else {
+                System.out.println("Node exists, skipping...");
+            }
+
+
+
+
+        /*int index = -1;
         if (subGroup.equals("")) {
             System.out.print("Node has no subGroup specified. nodeExist? ");
             for (int i = 0; i < amount; i++) {
@@ -253,7 +327,9 @@ public class ICEDesc extends BasePage {
                 fillDescription(subGroup, nodeName, text);
             } else {
                 System.out.println("Node exists, skipping...");
-            }
+            }*/
+
+            //==========================================================================
 
 /*        if (subGroup.equals("")){
             System.out.print("Node has no subGroup specified. nodeExist? ");
